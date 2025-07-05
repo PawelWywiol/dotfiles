f9d84fd88d2dce7a2077e68c1bfdff1587ff2de1
Paweł Wywioł <pawel.wywiol@gmail.com>
2025-07-06 00:24:47 +0200

    chore: update lazyvim packages

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = {
+      adapters = {
+        ["neotest-vitest"] = {},
+      },
+    },
+  },
+}
